c7aaefcd276a1a9c5eee1f6e3010f42c06c90850
Srinjoy Sur <srinjoy_sur@epam.com>
2025-08-19 15:49:18 +0530

    chore: corrected vocab for features in Registration tests for API created

--- com.contact_list/src/test/java/com/contact_list/api/stepdefinitions/RegistrationSteps.java
+++ com.contact_list/src/test/java/com/contact_list/api/stepdefinitions/RegistrationSteps.java
@@ -15,12 +15,12 @@ import static io.restassured.RestAssured.given;
 public class RegistrationSteps {
     private Response response;
     private RequestSpecification requestSpecification;
-    @Given("User has endpoint for Registration")
+    @Given("User has request specifications for Registration")
     public void userHasEndpointForRegistration() {
         // Write code here that turns the phrase above into concrete actions
         requestSpecification=RequestSpecificationManager.getRequestSpecification();
     }
-    @When("User sends a request with First Name {string}, Last Name {string}, Email {string} and Password {string}")
+    @When("User sends a post request with First Name {string}, Last Name {string}, Email {string} and Password {string}")
     public void userSendsARequestWithFirstNameLastNameEmailAndPassword(String firstName, String lastName, String email, String password) {
         // Write code here that turns the phrase above into concrete actions
         Register register=new Register.RegisterBuilder().withFirstName(firstName).withLastName(lastName).withEmail(email).withPassword(password).build();
--- com.contact_list/src/test/resources/features/api/Registration.feature
+++ com.contact_list/src/test/resources/features/api/Registration.feature
@@ -1,8 +1,8 @@
 Feature: Registration Features for API
   Background:
-    Given User has endpoint for Registration
+    Given User has request specifications for Registration
     Scenario Template: Valid Registration
-      When User sends a request with First Name "<firstName>", Last Name "<lastName>", Email "<email>" and Password "<password>"
+      When User sends a post request with First Name "<firstName>", Last Name "<lastName>", Email "<email>" and Password "<password>"
       Then User receives Success Response
       Examples:
       |firstName|lastName|email|password|
